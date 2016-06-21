32892143aba9dad0e47ffef4d3ef600cf57985ed
mjkong <mooje.kong@gmail.com>
2016-06-21 23:03:48 +0900

    commit test

---
+++ BACDev/src/org/mjkong/StringUtil.java
@@ -0,0 +1,12 @@
+package org.mjkong;
+
+/**
+ * Created by mjkong on 6/21/16.
+ */
+public class StringUtil {
+
+    public static String replace(String orig, String target){
+
+        return "";
+    }
+}
---
+++ BACDev/src/org/mjkong/Test.java
@@ -0,0 +1,10 @@
+package org.mjkong;
+
+/**
+ * Created by mjkong on 6/21/16.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
